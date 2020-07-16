d5aab51a7cdc1dbb3931355edded3a869cd7cd67
Jonathan Dorety <jdorety@flur.ee>
2020-07-16 11:21:29 -0400

    App will set route based on JWT on mount

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Switch, Route, useHistory } from "react-router-dom";
+import jwt from "jsonwebtoken";
 import logo from "./logo.svg";
 import Dashboard from "./components/dashboard/Dashboard";
 import AuthForm from "./components/Authentication/AuthForm";
 import "./App.css";
 
 function App() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      const { iss, exp } = jwt.decode(token);
+      const now = Date.now();
+      if (iss === "example/mdm" && exp > now) {
+        history.push("/dash");
+      } else {
+        localStorage.removeItem("authToken");
+        history.push("/login");
+      }
+    }
+  });
+
   return (
     <div className="App">
       <Switch>
