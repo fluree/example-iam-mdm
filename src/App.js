2ac33dfe7000461114fe95d5722a4b6bc7116f9d
Jonathan Dorety <jdorety@flur.ee>
2020-07-15 20:39:09 -0400

    Routes for auth-forms / dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import Dashboard from "./components/dashboard/Dashboard";
+import AuthForm from "./components/Authentication/AuthForm";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Switch>
+        <Route path="/dash">
+          <Dashboard />
+        </Route>
+        <Route path="/login">
+          <AuthForm />
+        </Route>
+        <Route path="/register">
+          <AuthForm register />
+        </Route>
+      </Switch>
     </div>
   );
 }
